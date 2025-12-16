1d46be3109f9ae6f90bcf9c3e0c76f438f3dccb9
antoine <antoine.frayard@gmail.com>
2025-12-16 09:48:13 +0100

    commit last changes
    UART_TX works fine
    
    ADC convertion to BCD for UART -> OK -> must change conversion to BCD methods
    
    UART_RX -> WIP

---
+++ TP1.X/build/default/debug/_ext/2101421332/tuto_pic2022.o.d
@@ -0,0 +1,4 @@
+build/default/debug/_ext/2101421332/tuto_pic2022.o:  \
+	//vfiler-ad-etu.ad.unistra.fr/afrayard/Bureau/tuto_pic2022.asm  \
+	//VFILER-AD-ETU.AD.UNISTRA.FR/AFRAYARD/Bureau/p16f877.inc  \
+
